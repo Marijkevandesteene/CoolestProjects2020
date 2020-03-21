e25f77dc878f0e555a58b9615f7bd2140cd2af6b
Marijkevandesteene <Marijkevandesteene@hotmail.com>
2020-03-21 11:01:23 +0100

    +speech bubble bij dwerg

---
+++ francis unity_123/Assets/dwerg.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dwerg : MonoBehaviour
+{
+    public GameObject ObjectToLookAt;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(ObjectToLookAt.transform);
+    }
+}
